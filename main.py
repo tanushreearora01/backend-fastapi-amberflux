67bf9935f29ade64abd137093dd6f0c328d3e1e0
Tanushree Arora <aroratanushree.28@gmail.com>
2025-09-08 23:12:49 +0530

    classes for document, document and health added

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-
-
+from datetime import datetime
 
 app = FastAPI()
+
+class DocumentResponse(BaseModel):
+    id: str
+    filename: str
+    size: int
+    page_count: int
+    status: str
+    created_at: datetime
+
+    class Cofing:
+        from_attributes = True
+
+class DocumentPageResponse(BaseModel):
+    page_number: int 
+    text: str
+    chunks: list = []
+
+class HealthResponse(BaseModel):
+    status: str
+    fastapi_version: str
+    db_connection: bool 
 
 @app.get('/')
 def read_root():
