d8a030a52b536b6ef14f198a9befaa617ab881c9
Tanushree Arora <aroratanushree.28@gmail.com>
2025-09-10 03:21:24 +0530

    changes made due to deployment issues

--- database.py
+++ database.py
@@ -1,9 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
  
-URL_Database = "postgresql://postgres:12345@localhost:5432/document_library"
-
-engine = create_engine(URL_Database)
+engine = create_engine(os.getenv("URL_Database"))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
