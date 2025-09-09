2d477aad888ec0afda02979f1208491e57144459
Tanushree Arora <aroratanushree.28@gmail.com>
2025-09-10 04:20:51 +0530

    added pyscopg

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
  
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://postgres:12345@localhost:5432/document_library")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:12345@localhost:5432/document_library")
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+psycopg://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
